60ce3d9edcc4e011a4b822106a43989bac173d4f
sylviawangfr <32857916+sylviawangfr@users.noreply.github.com>
2018-03-12 11:19:45 +0800

    add karma-junit-reporter (#558)

--- karma.conf.js
+++ karma.conf.js
@@ -87,6 +87,16 @@ module.exports = function(config) {
       foldAll: false, // reports start folded (only with preserveDescribeNesting)
     },
 
+    junitReporter: {
+          outputDir: 'junit', // results will be saved as $outputDir/$browserName.xml
+          outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+          suite: '', // suite will become the package name attribute in xml testsuite element
+          useBrowserName: true, // add browser name to report and classes names
+          nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+          classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+          properties: {} // key value pair of properties to add to the <properties> section of the report
+        },
+
 
     // web server port
     port: 9876,
